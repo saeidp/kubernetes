9a4c0c5c9ca8dc7d1f50518b9f646b74da941a0c
saeid pahlevan <saeidpahlevan@hotmail.com>
2023-11-17 07:30:28 +0800

    feat(cdk8s): add deployment and service

--- cdk8s/my-hello-world/main.ts
+++ cdk8s/my-hello-world/main.ts
@@ -1,15 +1,42 @@
 import { Construct } from 'constructs';
 import { App, Chart, ChartProps } from 'cdk8s';
-
-export class MyChart extends Chart {
-  constructor(scope: Construct, id: string, props: ChartProps = { }) {
+import { KubeDeployment, KubeService, IntOrString } from './imports/k8s';
+export class HelloChart extends Chart {
+  constructor(scope: Construct, id: string, props: ChartProps = {}) {
     super(scope, id, props);
 
-    // define resources here
+    const helloLabel = { app: 'hello-k8s' }
+
+    new KubeDeployment(this, 'deployment', {
+      spec: {
+        replicas: 2,
+        selector: { matchLabels: helloLabel },
+        template: {
+          metadata: { labels: helloLabel },
+          spec: {
+            containers: [
+              {
+                name: 'my-hello-world',
+                image: 'my-hello-world',
+                ports: [{ containerPort: 80 }],
+                imagePullPolicy: 'Never'
+              }
+            ]
+          }
+        }
+      }
+    });
+    new KubeService(this, 'service', {
+      spec: {
+        type: 'NodePort',
+        ports: [{ port: 80, targetPort: IntOrString.fromNumber(80) }],
+        selector: helloLabel
+      }
+    })
 
   }
 }
 
 const app = new App();
-new MyChart(app, 'my-hello-world');
+new HelloChart(app, 'my-hello-world');
 app.synth();
